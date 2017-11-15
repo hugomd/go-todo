f98a1b81e7de49fb4773595090e3a25d057d3988
Hugo <h@hugo.sx>
2017-11-14 22:52:32 -0800

    Add defaults for CreatedAt and UpdatedAt times

--- models/task.go
+++ models/task.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+  "time"
   "github.com/satori/go.uuid"
   "github.com/jinzhu/gorm"
 )
@@ -8,11 +9,19 @@ import (
 // Task "Object
 type Task struct {
   ID  uuid.UUID   `json:"id"`
-  Title string  `json:"title"`
-  CreatedAt string  `json:"created_at"`
+  Title string  `json:"title" binding:"required"`
+  CreatedAt time.Time  `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
   Completed bool  `json:"completed"`
 }
 
 func (task *Task) BeforeCreate(scope *gorm.Scope) error {
-    return scope.SetColumn("ID", uuid.NewV4().String())
+  scope.SetColumn("CreatedAt", time.Now())
+  scope.SetColumn("ID", uuid.NewV4().String())
+  return nil
+}
+
+func (task *Task) BeforeUpdate(scope *gorm.Scope) error {
+  scope.SetColumn("UpdatedAt", time.Now())
+  return nil
 }
