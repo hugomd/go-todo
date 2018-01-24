a8492fe55432965fb0ee339ed0cbe75dc021c127
Hugo Müller-Downing <hugo@localz.co>
2018-01-24 20:04:30 +0000

    Use environment for database connection

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"os"
     "fmt"
     "log"
   _ "github.com/lib/pq"
@@ -12,17 +13,29 @@ import (
 var db *gorm.DB
 var err error
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 // Init creates a connection to mysql database and 
 // migrates any new models
 func Init() {
-  dbinfo := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
-    "hugo",
-    "",
-    "localhost",
-    "5432",
-    "tasks",
-  )
+	user := getEnv("PG_USER", "hugo")
+	password := getEnv("PG_PASSWORD", "")
+	host := getEnv("PG_HOST", "localhost")
+	port := getEnv("PG_PORT", "8080")
+	database := getEnv("PG_DB", "tasks")
 
+  dbinfo := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+    user,
+    password,
+    host,
+    port,
+    database,
+  )
 
   db, err = gorm.Open("postgres", dbinfo)
   if err != nil {
